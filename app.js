75a485d861be394e9a0ec604ae55bf91baf3a42d
RAKESH RANJAN <rakesh.ranjan2@microfocus.com>
2020-01-13 13:28:11 +0530

    modified index about sitemap

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
 
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname+'/about.html')))
+
+app.get('/sitemap', (req, res) => res.sendFile(path.join(__dirname+'/sitemap.html')))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
